b2a0c5d5a19eecb522e22444b3938d5e89ee22a4
Marcin <Marcin@KursGIT.pl>
2021-06-12 12:20:06 +0200

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MyComponentComponent } from './my-component/my-component.component';
+import { MyComp2Component } from './my-comp2/my-comp2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyComponentComponent,
+    MyComp2Component
   ],
   imports: [
     BrowserModule

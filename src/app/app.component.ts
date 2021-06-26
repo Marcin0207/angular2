08fabaa2dbde8092f05e4a15662c72a715e1b137
Marcin <Marcin@KursGIT.pl>
2021-06-26 09:02:35 +0200

    zad5 switch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,30 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styles: [`   .tabs-selection {background-color: #ddd; padding-left: 15px; cursor: pointer;} `]
 })
 export class AppComponent {
   title = 'my-project';
-
   exists = true;
+  tab = 2;
+
   toggleExists(){
     this.exists = !this.exists;
+  }
+
+  topMovies = [
+    {title: 'The Shawshank Redemption', director: 'Frank Darabont'},
+    {title: 'The Godfather', director: 'Francis Ford Coppola'},
+    {title: 'The Godfather: Part II', director: 'Francis Ford Coppola'},
+    {title: 'The Dark Knight', director: 'Christopher Nolan'},
+    {title: '12 Angry Man', director: 'Sidney Lumet'}
+  ];
+
+
+  setTab(num: number) {
+    this.tab = num;
+  }
+  isSelected(num: number){
+    return this.tab ===num;
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { MyComponentComponent } from './my-component/my-component.component';
 import { MyComp2Component } from './my-comp2/my-comp2.component';
+import { AppTabComponentComponent } from './app-tab-component/app-tab-component.component';
+import { AppTabComponent } from './app-tab/app-tab.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MyComponentComponent,
-    MyComp2Component
+    MyComp2Component,
+    AppTabComponentComponent,
+    AppTabComponent
   ],
   imports: [
     BrowserModule
